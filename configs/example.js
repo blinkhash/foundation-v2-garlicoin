f712a6430854ef0c0aad698c83ecfbf502dc6e8c
blinkhash <blinkhashpool@gmail.com>
2022-06-16 16:43:50 -0400

    Finish up unit tests, small changes

--- configs/example.js
+++ configs/example.js
@@ -1,6 +1,6 @@
 /*
  *
- * Example (Bitcoin)
+ * Example (Garlicoin)
  *
  */
 
@@ -49,15 +49,15 @@ config.primary.address = '[address]';
 
 // Coin Configuration
 config.primary.coin = {};
-config.primary.coin.name = 'Bitcoin';
-config.primary.coin.symbol = 'BTC';
+config.primary.coin.name = 'Garlicoin';
+config.primary.coin.symbol = 'GRLC';
 
 // Daemon Configuration
 config.primary.daemons = [];
 
 const daemons1 = {};
 daemons1.host = '127.0.0.1';
-daemons1.port = 8332;
+daemons1.port = 42069;
 daemons1.username = '';
 daemons1.password = '';
 config.primary.daemons.push(daemons1);
